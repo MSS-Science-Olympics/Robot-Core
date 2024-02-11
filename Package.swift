85c1631eebae3479d697ee54fbbf08379623d6c2
0x41c <56287516+0x41c@users.noreply.github.com>
2024-02-10 20:40:20 -0800

    Setup dependancies in preparation

--- Package.swift
+++ Package.swift
@@ -4,8 +4,20 @@ import PackageDescription
 
 let package = Package(
     name: "arpy",
+    platforms: [.macOS(.v14)],
+    dependencies: [
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.90.0"),
+        .package(url: "https://github.com/uraimo/SwiftyGPIO.git", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0")
+    ],
     targets: [
         .executableTarget(
-            name: "arpy"),
+            name: "arpy",
+            dependencies: [
+                .product(name: "Vapor", package: "vapor"),
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                "SwiftyGPIO"
+            ]
+        ),
     ]
 )
---
+++ Sources/Arpy.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//
+//
+//  Created by 0x41c on 2024-02-10.
+//
+
+import ArgumentParser
+
+@main
+struct Arpy: AsyncParsableCommand {
+    mutating func run() async throws {
+        print("Hello World")
+    }
+}
